ecb1dbdcd848a30bb33b4572f922976f199f1ebf
calvinearnshaw <calvin.earnshaw@outlook.com>
2023-06-27 18:38:48 +0100

    Full React Tutorial #10 - improving home screen and adding blog entries

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,20 @@
 import { useState } from 'react';
 
 const Home = function() {
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-
-    const handleClick = function() {
-        setName('luigi');
-        setAge(20);
-    }
+    const [blogs, setBlogs] = useState([
+        {title: "My new website!", body: "Lorem ipsum dolor sit amet...", author: "Mario", id: 1},
+        {title: "Welcome party", body: "Lorem ipsum dolor sit amet...", author: "Yoshi", id: 2},
+        {title: "Web dev top tips", body: "Lorem ipsum dolor sit amet...", author: "Mario", id: 3}
+    ]);
 
     return (
-        <div className='home'>
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click Me!</button>
+        <div className="home">
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id} >
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
     );
 }
