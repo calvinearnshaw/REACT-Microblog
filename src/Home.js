1b3c779b7fcfb56fa949c774c13c5f1524e9b890
calvinearnshaw <calvin.earnshaw@outlook.com>
2023-06-28 11:12:42 +0100

    Tutorial #19: Handling Fetch Errors

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,31 @@ import BlogList from "./BlogList";
 const Home = function() {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(function() {
         fetch('http://localhost:8000/blogs')
             .then(res => {
-                return res.json();
+                if (!res.ok) {
+                    throw Error('Could not fetch data for resource.')
+                } else {
+                    return res.json();
+                }
             })
             .then(data => {
                 setIsPending(false);
+                setError(null);
                 setBlogs(data);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
             })
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
